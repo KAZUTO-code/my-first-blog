e8e4ff2eb87481f9bdb7810f743ce03eda6b6717
KAZUTO-code <kazuto.hirao.1073@gmail.com>
2020-02-17 19:08:08 +0900

    Add pagination and Revise design.

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
-from PIL import Image
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    page_obj = paginate_query(request, posts, settings.PAGE_PER_ITEM)   # ページネーション
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -89,3 +91,15 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+# ページネーション用に、Pageオブジェクトを返す。
+def paginate_query(request, queryset, count):
+    paginator = Paginator(queryset, count)
+    page = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginatot.page(paginator.num_pages)
+    return page_obj
